dc671ea500c52da0a4a098758afbc27f146c1fd7
Zach Newell <new14008@byui.edu>
2019-04-01 13:34:30 -0600

    fixing main.js

--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 function getItems() {
     $(document).ready(function () {
         $.getJSON("/getTodoList", function (data) {
+            $(JSON.stringify(data)).appendTo("todoList");
             $.each(data.items, function(i, item) {
                 display = "<h3>" + item.name + "</h3>" +
                            "<p>" + item.due_date + "</p><br/>";
