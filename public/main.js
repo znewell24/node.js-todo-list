025a3fa5b7adf1d160012076fe2d70b6283a2b2f
Zach Newell <new14008@byui.edu>
2019-04-01 14:04:25 -0600

    fixing main.js

--- public/main.js
+++ public/main.js
@@ -1,18 +1,18 @@
 function getItems() {
     $(document).ready(function () {
         $.getJSON("/getTodoList", function (data) {
-            $(data).appendTo("#todoList");
-            $.each(data, function(i, item) {
-                JSON.stringify(item);
-                display = '<p>' + item.name + '</p>';
-                display = '<p>' + item.due_date + '</p><br/>';
-                $(display).appendTo("#todoList");
-            }); 
+            $(JSON.stringify(data)).appendTo("#todoList");
+           // $.each(data, function(i, item) {
+               // JSON.stringify(item);
+               // display = '<p>' + item.name + '</p>';
+               // display = '<p>' + item.due_date + '</p><br/>';
+               // $(display).appendTo("#todoList");
+           // }); 
             // document.getElementById("todoList").innerHTML = data;
         });
     });
 }
 
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-})
+// $('#myModal').on('shown.bs.modal', function () {
+//   $('#myInput').trigger('focus')
+// });
