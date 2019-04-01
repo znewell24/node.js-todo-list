34e8fd97f8463091de2e54ce23423b2aa7eba0c0
Zach Newell <new14008@byui.edu>
2019-04-01 13:27:18 -0600

    fixing main.js

--- public/main.js
+++ public/main.js
@@ -2,11 +2,11 @@ function getItems() {
     $(document).ready(function () {
         $.getJSON("/getTodoList", function (data) {
             var obj = JSON.parse(data);
-            var display = "";
+            var display = " ";
 
-            $.each(data, function(i, item) {
-                display += "<h3>" + data[i].name + "</h3>" +
-                           "<p>" + data[i].due_date + "</p>";
+            $.each(data.items, function(i, item) {
+                display += "<h3>" + data.items[i].name + "</h3>" +
+                           "<p>" + data.items[i].due_date + "</p>";
             }); 
 
             document.getElementById("todoList").innerHTML = display;
